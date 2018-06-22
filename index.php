4960c558f477a889c7adb9b578628b6d91b71543
Гайни <shakurovga@gmail.com>
2018-06-22 19:28:41 +0600

    База

--- index.php
+++ index.php
@@ -1,40 +1,10 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-$api = new \Yandex\Geo\Api();
-
-// Можно искать по точке
-//$api->setPoint(30.5166187, 50.4452705);
-
-// Или можно икать по адресу
-if (!empty($_GET['searchText'])) {
-
-
-    $api->setQuery($_GET['searchText']);
-
-// Настройка фильтров
-    $api
-        ->setLang(\Yandex\Geo\Api::LANG_RU)// локаль ответа
-        ->load();
-
-    $response = $api->getResponse();
-    $response->getFoundCount(); // кол-во найденных адресов
-    $response->getQuery(); // исходный запрос
-    $response->getLatitude(); // широта для исходного запроса
-    $response->getLongitude(); // долгота для исходного запроса
-
-    echo '<h4>'.$_GET['searchText'].'</h4>';
-
-// Список найденных точек
-    $collection = $response->getList();
-    foreach ($collection as $item) {
-        echo '<b>' . $item->getAddress() . '</b><br />'; // вернет адрес
-        echo 'Широта - '. $item->getLatitude() . '<br />'; // широта
-        echo 'Долгота - '. $item->getLongitude() . '<br />'; // долгота
-    }
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Яндекс Геолокация</title>
+</head>
+<body>
 
 <form action="" method="GET">
     Найти:
@@ -43,3 +13,34 @@ if (!empty($_GET['searchText'])) {
     </p>
     <p><input type="submit" value="Найти"/></p>
 </form>
+
+<?php
+
+require_once 'vendor/autoload.php';
+
+$api = new \Yandex\Geo\Api();
+
+if (!empty($_GET['searchText'])) {
+    $api->setQuery($_GET['searchText']);
+
+// Настройка фильтров
+    $api
+        ->setLang(\Yandex\Geo\Api::LANG_RU)// локаль ответа
+        ->load();
+
+    $response = $api->getResponse();
+
+    echo '<h4>' . $_GET['searchText'] . '</h4>';
+
+// Список найденных точек
+    $collection = $response->getList();
+    foreach ($collection as $item) {
+        echo '<b>' . $item->getAddress() . '</b><br />'; // вернет адрес
+        echo 'Широта - ' . $item->getLatitude() . '<br />'; // широта
+        echo 'Долгота - ' . $item->getLongitude() . '<br />'; // долгота
+    }
+}
+?>
+
+</body>
+</html>
